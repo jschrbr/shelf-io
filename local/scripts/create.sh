958b7e4b7144aded5084bff7b5d9345ad3e711c4
jschrbr <schrbrr@gmail.com>
2020-07-22 06:56:34 +1000

    Cleanup

--- local/scripts/create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if command -v heroku
-then
-npm i -g heroku 
-fi
-heroku login
-heroku create example
-heroku buildpacks:set https://github.com/chibat/heroku-buildpack-deno.git
-heroku addons:create jawsdb
