9862e9c8215bde9b4550bbc4803f0205b7cf5c0a
jschrbr <schrbrr@gmail.com>
2020-07-23 09:08:13 +1000

    Added sensor print for debugging

--- IoT/flash/main.py
+++ IoT/flash/main.py
@@ -5,31 +5,37 @@ import time
 import math
 import api
 
-massFactor = 4400
+massFactor = 4000
 scales = Scales(d_out=22, pd_sck=21)
 delay_read = 0.1
 delay_post = 0.8
 
 
-def getCount():
+def readScale():
     val = abs(scales.stable_value())
-    val = math.floor(val/massFactor)
+    return val
+
+
+def getCount(raw_val):
+    val = math.floor(raw_val/massFactor)
     val = str(max(0, val))
     return val
 
 
 partId = views.get_partBtn()
 
-prev_val = getCount()
+prev_val = getCount(readScale())
 scales.tare()
-prev_val = getCount()
+prev_val = getCount(readScale())
 
 
 while True:
-    val = getCount()
+    raw_val = readScale()
+    val = getCount(raw_val)
+    lcd.text(lcd.CENTER, 0, str(raw_val))
     if (val != prev_val):
         time.sleep(delay_post)
-        val = getCount()
+        val = getCount(readScale())
         if (val != prev_val):
             prev_val = val
             api.updateParts(partId, val)
